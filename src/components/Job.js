5f0f3839bfb5329f3db350b799e9a246ee99cc48
lanbow93 <the.doctor1501@gmail.com>
2023-02-06 15:34:12 -0600

    Job listing added

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,7 +1,13 @@
 function Job(props){
     console.log(props.data)
+    const job = props.data
     return <div className="jobCard">
-        <h2>{props.data.title}</h2>
+        <h2>{job.title}</h2>
+        <h4>{job.company}</h4>
+        <h4>{job.date}</h4>
+        <h4>{job.location}</h4>
+        
+        {job.details.map(detail => <p>{detail}</p>)}
     </div>
 }
 
