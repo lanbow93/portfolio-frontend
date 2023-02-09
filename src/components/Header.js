7e7f76ffc0f4388ed1fb9187d2e39a7c5ad8ad6d
Brandon Haupt <haupt.brandon@gmail.com>
2023-02-09 09:16:06 -0600

    update 1.9
    - added animate.css
    - added a fall in animation for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,17 @@ export default function Header(props){
 
     return(
         <header>
+             <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+            />
             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F64.media.tumblr.com%2Fa39911173ad8fd36f451961c23f504b9%2Ftumblr_of1p3313Al1rldv4go1_1280.gif&f=1&nofb=1&ipt=0bee84832b687b114a5f8b5ee78be70e11c5e37c102491eca4779bafc28e9239&ipo=images" alt="profile" className="header-img"/>
 
             <div className="profile-image-circle">
                 <img src={ProfilePicture} alt="profile" className="profile-img"/>
             </div>
             <nav className="nav">
-                <section>    
+                <section className="animate__animated animate__fadeInDown">    
                     <p className="myname">Brandon Haupt</p>
                     <p className="whoIAm">Designer | Developer</p>
                 </section>
