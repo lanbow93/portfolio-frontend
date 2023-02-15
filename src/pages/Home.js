c55dc9d28b15f67c5ec33ad319f19748ba8984d6
Brandon Haupt <haupt.brandon@gmail.com>
2023-02-14 18:39:27 -0600

    Update 2.4
    - added a div to better section off the projects and skills
    - changed css to change it back to being responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,49 +12,54 @@ export default function Home(props){
             </section>
 
             <section className="skills">
-                {/* <h3>Skills</h3> */}
-                <span className="UI-section">
-                    <h3>UX/UI DESIGN</h3>
-                    <p>APP DESIGN</p>
-                    <p>FIGMA</p>
-                    <p>WIREFRAME</p>
-                    <p>GOOGLE FONTS</p>
-                    <p>LOW/HIGH FIDELITY MOCKUPS</p>
-                </span>
-                <span className="Frontend-section">
-                    <h3>FRONTEND</h3>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>SASS</p>
-                    <p>JS</p>
-                    <p>REACT</p>
-                    <p>NODEJS</p>
-                </span>
-                <span className="other-section">
-                    <h3>OTHER</h3>
-                    <p>D&D</p>
-                    <p>EVERQUEST</p>
-                    <p>MINECRAFT</p>
-                    <p>BLACKSMITHING</p>
-                </span>
+                <h2>Technical Skills</h2>
+                <div className="skill-container">
+                    <span className="UI-section">
+                        <h3>UX/UI DESIGN</h3>
+                        <p>APP DESIGN</p>
+                        <p>FIGMA</p>
+                        <p>WIREFRAME</p>
+                        <p>GOOGLE FONTS</p>
+                        <p>LOW/HIGH FIDELITY MOCKUPS</p>
+                    </span>
+                    <span className="Frontend-section">
+                        <h3>FRONTEND</h3>
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>SASS</p>
+                        <p>JS</p>
+                        <p>REACT</p>
+                        <p>NODEJS</p>
+                    </span>
+                    <span className="other-section">
+                        <h3>OTHER</h3>
+                        <p>D&D</p>
+                        <p>EVERQUEST</p>
+                        <p>MINECRAFT</p>
+                        <p>BLACKSMITHING</p>
+                    </span>
+                </div>
             </section>
 
             <section className="project">
-                {projects.map((project) => (
-                    <div className="project-list" id={project.projectid}>
-                        <h3>{project.name}</h3>
-                        {/* <span></span> */}
-                        <img src={project.image} alt={project.name}/>
-                        <div className="project-buttons">
-                        <a href={project.git}>
-                            <button>GitHub</button>
-                        </a>
-                        <a href={project.live}>
-                            <button>Live Site</button>
-                        </a>
+                <h2>Projects</h2>
+                <div className="project-container">
+                    {projects.map((project) => (
+                        <div className="project-list" id={project.projectid}>
+                            <h3>{project.name}</h3>
+                            {/* <span></span> */}
+                            <img src={project.image} alt={project.name}/>
+                            <div className="project-buttons">
+                            <a href={project.git}>
+                                <button>GitHub</button>
+                            </a>
+                            <a href={project.live}>
+                                <button>Live Site</button>
+                            </a>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </section>  
         </main>
 
