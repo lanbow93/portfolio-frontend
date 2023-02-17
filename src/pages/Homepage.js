88fa3fcd1b4b79feb019c98d7343ce11f1382cf5
lanbow93 <the.doctor1501@gmail.com>
2023-02-17 14:10:02 -0600

    Added in noreferrer and more adjustments for icons

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -9,16 +9,16 @@ function Homepage(props) {
             <img src={data.headshot} alt="Headshot of developer cropped in a circle"/>
         </div>
         <div className="section contacts">
-            <h1>Contact Me!</h1>
+            <h2>Contact Me!</h2>
             <p>{data.contact.email}</p>
             <div className="iconArea">
                 <div className="linkedin">
-                    <a href={data.contact.linkedin} target="_blank" rel="noopener">
+                    <a href={data.contact.linkedin} target="_blank" rel="noreferrer">
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" alt="LinkedIn Icon that links to LinkedIn profile" />
                     </a>
                 </div>
                 <div className="github">
-                    <a href={data.contact.github} target="_blank" rel="noopener">
+                    <a href={data.contact.github} target="_blank" rel="noreferrer">
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="Github Icon that links to Github profile" width="90rem"/>
                     </a>
                 </div>
