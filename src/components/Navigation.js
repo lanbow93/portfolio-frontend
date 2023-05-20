ff17c3f4b1945ec7ed0f2bb68f075466fc2e7b1a
lanbow93 <the.doctor1501@gmail.com>
2023-05-19 21:36:55 -0500

    Changed navigation text

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 function Navigation(props) {
     return <nav>
-       <Link to="/" ><img src="https://static.wixstatic.com/media/8ee8bf_ba71b913edb14fbab458507a9c66dcc0~mv2.png" alt="Logo with the initials LBJ" /></Link>
        <Link to="/" className="navLink">Home</Link>
        <Link to="/about" className="navLink">About</Link>
        <Link to="/projects" className="navLink">Projects</Link>
+       <Link to="/contact" className="navLink">Contact</Link>
     </nav>
 };
 export default Navigation;
