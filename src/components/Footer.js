8ae945f01776d496370a3920f1e3161c970c0cb7
lanbow93 <the.doctor1501@gmail.com>
2023-05-26 12:57:49 -0500

    Mobile homepage reconfigured

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
 function Footer(props) {
     
     return <div className="footer">
-        <marquee behavior="scroll" direction="left" scrollamount="4">Skill List  : 
-        <img className="skillIconAjax" src="https://static.wixstatic.com/media/8ee8bf_c6bdde2c6ee84d5ca88b2eb6282bbba3~mv2.png" alt=" icon for AJAX" /> {/* Ajax */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg" alt=" icon for Bootstrap"/> {/* Bootstrap */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt=" icon for CSS"/> {/* CSS */}
-        <img className="skillIconAjax" src="https://static.wixstatic.com/media/8ee8bf_f727f0135173425c84e89d77d638c940~mv2.png" alt=" icon for EJS" /> {/* EJS */}
-        <img className="skillIconAjax" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt=" icon for Express" /> {/* Express */}
-        <img className="skillIconAjax" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" alt=" icon for Git" /> {/* Git */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt=" icon for HTML5" /> {/* Html */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt=" icon for JavaScript" /> {/* JavaScript */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg" alt=" icon for jQuery" /> {/* jQuery */}
-        <img className="skillIconMongo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt=" icon for Mongo" /> {/* Mongo */}
-        <img className="skillIconMongoose" src="https://static.wixstatic.com/media/8ee8bf_1ef5a049e94e4202b9f4a161fb2427f3~mv2.png" alt=" icon for Mongoose" /> {/* Mongoose */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt=" icon for React" /> {/* React */}
-        <img className="skillIconScrum" src="https://static.wixstatic.com/media/8ee8bf_837eba9587cc49d1a3c5619880e457f9~mv2.jpg" alt=" icon for Scrum" /> {/* Scrum */}
-        <img className="skillIconAjax" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain-wordmark.svg" alt=" icon for Trello" /> {/* Trello */}
-        <img className="skillIcon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt={`icon for TypeScript`} /> {/* TypeScript */}
-        </marquee>
+
+                <div className="iconArea">
+                <div className="contact">
+                <h3>Contact Me!</h3>
+                <p>LanceBowersJr1434@gmail.com</p>
+                </div>
+                    <div className="linkedin">
+                        <a href="https://www.linkedin.com/in/lanbow/" target="_blank" rel="noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" alt="LinkedIn Icon that links to LinkedIn profile" />
+                        </a>
+                    </div>
+                    <div className="github">
+                        <a href="https://github.com/lanbow93" target="_blank" rel="noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="Github Icon that links to Github profile" width="90rem"/>
+                        </a>
+                    </div>
+                </div>
     </div>
 }
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,34 +2,46 @@ import { useLoaderData } from "react-router-dom"
 function Homepage(props) {
     const data = useLoaderData()
     return <div className="homepage">
-        <div className="statement">
-            <h2>Lance Bowers Jr</h2>
+        <div className="name">
+            <h1>Lance Bowers Jr</h1>
         </div>
-        <div className="section headshot">
-            <img src={data.headshot} alt="Headshot of developer cropped in a circle"/>
-        </div>
-        <div className="section contacts">
-            <h2>Contact Me!</h2>
-            <p>{data.contact.email}</p>
-            <div className="iconArea">
-                <div className="linkedin">
-                    <a href={data.contact.linkedin} target="_blank" rel="noreferrer">
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" alt="LinkedIn Icon that links to LinkedIn profile" />
-                    </a>
+        
+        <div className="intro">
+            <div className="headshot">
+                <img src={data.headshot} alt="Headshot of developer cropped in a circle"/>
+            </div>
+            <div className="information">
+                <div className="description">
+                    <h3>Full-Stack Developer</h3>
+                    <p>Building responsive, accessible web applications with Express, JavaScript/TypeScript, and React</p>
                 </div>
-                <div className="github">
-                    <a href={data.contact.github} target="_blank" rel="noreferrer">
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="Github Icon that links to Github profile" width="90rem"/>
-                    </a>
+                <div className="contact">
+                    <h3>Contact Me</h3>
+                    <p>{data.contact.email}</p>
+                </div>
+                <div className="iconArea">
+                    <div className="linkedin">
+                        <a href={data.contact.linkedin} target="_blank" rel="noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg" alt="LinkedIn Icon that links to LinkedIn profile" />
+                        </a>
+                    </div>
+                    <div className="github">
+                        <a href={data.contact.github} target="_blank" rel="noreferrer">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="Github Icon that links to Github profile" width="90rem"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
         <div className="section bio">
+            <h2>About</h2>
             <p>{data.bio[0].split(".")[0]}.</p>
+            <br />
             <p>{data.bio[0].split(".")[1]}.</p>
+            <br />
             <p>{data.bio[1].split(".")[0]}.</p>
-            <p>{data.bio[1].split(".")[1]}.</p>
         </div>
+
     </div>
 } 
 export default Homepage
